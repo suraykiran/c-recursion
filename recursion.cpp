5c91f9658f1cf2cc14ebfccd38e899f8a69f1cf7
suraykiran <106419106+suraykiran@users.noreply.github.com>
2023-02-06 20:50:02 +0530

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -1,5 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
+int lastoccrance(int *a,int n,int k)
+{
+    if(n==0) return -1;
+    int ind= lastoccrance(a+1,n-1,k);
+    if(ind == -1)
+    {
+        if(a[0]==k)
+            return 0;
+        else 
+        return -1;
+    }
+    return ind+1;
+}
+int firstoccrance(int *a,int n,int k)
+{
+    if(n==0)
+        return -1;
+    if(a[0]==k)
+        return 0;
+    int ind =firstoccrance(a+1,n-1,k);
+    if(ind== -1)
+        return -1;
+    return ind+1;
+}
+int isarysortednotexatraspace(int *a,int n)
+{
+    if(n==1) return 1;
+    if(a[0]<a[1])
+        return isarysortednotexatraspace(a+1,n-1);
+    else
+        return -1;
+}
+int isarysorted(int *a,int n,int i)
+{
+    if(i==n-1) return 1;
+    if(a[i]<a[i+1])
+     return isarysorted(a,n,i+1);
+    else
+     return -1;
+}
 int issorted(int *a,int n,int k,int out[],int i,int j)
 {
     if(i>=n) return j;
@@ -23,4 +63,8 @@ int main()
     {
         cout<<out[i]<<" ";
     }
+    cout<<isarysorted(arr,n,0);
+    cout<<isarysortednotexatraspace(arr,n);
+    cout<<firstoccrance(arr,n,key);
+    cout<<lastoccrance(arr,n,key);
 }
