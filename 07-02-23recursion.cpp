d7df97fdc1f043e65f36af34cc4c60082d38ddf0
suraykiran <106419106+suraykiran@users.noreply.github.com>
2023-02-07 18:30:58 +0530

    Create 07-02-23recursion.cpp

---
+++ 07-02-23recursion.cpp
@@ -0,0 +1,26 @@
+//using recursion
+#include<bits/stdc++.h>
+using namespace std;
+void printsubsets(int ind,int a[],int n,vector<int>v)
+{
+     if(ind == n){
+        for(auto x:v)
+            cout<<x;
+      cout<<endl;
+     return;}
+
+     v.push_back(a[ind]);
+     printsubsets(ind+1,a,n,v);
+     v.pop_back();
+     printsubsets(ind+1,a,n,v); 
+}
+
+int main()
+{
+    int arr[]={1,2,3};
+    int n=sizeof(arr)/sizeof(int);
+    vector<int>d;
+    printsubsets(0,arr,n,d);
+
+}
+//using powerset
